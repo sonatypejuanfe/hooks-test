3a5c453ed9fd5ea24642d277206fb12aa3ef75ff
Juan Morales <jmorales@sonatype.com>
2019-01-10 16:42:28 -0500

    Lets add some minor detail

--- src/main/java/com/sonatype/iday/App.java
+++ src/main/java/com/sonatype/iday/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, this is the development improvement!" );
+      System.out.println( "Hello World, this is the development improvement!" );
+      System.out.println( "Greetings from Mars!" );
     }
 }
