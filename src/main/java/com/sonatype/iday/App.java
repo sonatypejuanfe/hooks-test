461beb40ba3923bec76ce8ab9d7e08e4a598a7f0
Juan Morales <jmorales@sonatype.com>
2019-01-10 16:25:44 -0500

    This is the first commit to test the status of commits

---
+++ src/main/java/com/sonatype/iday/App.java
@@ -0,0 +1,13 @@
+package com.sonatype.iday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World, this is the development improvement!" );
+    }
+}
